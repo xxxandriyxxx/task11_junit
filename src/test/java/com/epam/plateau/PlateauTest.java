47954b50a434c19ca34d4abf8efb347ec5930ef1
Andriy <andriymattt@gmail.com>
2019-12-08 23:34:54 +0200

    feature/test: add tests to the class PlateauTest

--- src/test/java/com/epam/plateau/PlateauTest.java
+++ src/test/java/com/epam/plateau/PlateauTest.java
@@ -1,4 +1,27 @@
 package com.epam.plateau;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PlateauTest {
+
+    @Test
+    void testPlateau() {
+        int[] values = {5, 5, 4, 2, 3, 3, 3, 3, 3, 1, 2};
+        Plateau plateau = new Plateau(values);
+        int[] longestPlateau = new int[]{2, 3, 3, 3, 3, 3, 1};
+        assertArrayEquals(plateau.getLongestPlateau(), longestPlateau, "Longest plateaus are not equal");
+    }
+
+    @Test
+    void testLength() {
+        int[] values = {5, 5, 4, 2, 3, 3, 3, 3, 3, 1, 2};
+        Plateau plateau = new Plateau(values);
+        if (plateau.getLongestPlateau().length != 10) {
+            fail("Longest plateau length != 10");
+        }
+    }
+
+
 }
