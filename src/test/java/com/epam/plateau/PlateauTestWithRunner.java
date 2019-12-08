4994a4491f0aafa38e2271c7ce832f10c542c2f6
Andriy <andriymattt@gmail.com>
2019-12-09 00:07:59 +0200

    feature/test: add parameterized tests to PlateauTestWithRunner class

--- src/test/java/com/epam/plateau/PlateauTestWithRunner.java
+++ src/test/java/com/epam/plateau/PlateauTestWithRunner.java
@@ -1,4 +1,48 @@
 package com.epam.plateau;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class PlateauTestWithRunner {
+    @Parameterized.Parameter(0)
+    public int longestPlateauLength;
+    @Parameterized.Parameter(1)
+    public int longestPlateauStartIndex;
+    @Parameterized.Parameter(2)
+    public int longestPlateauEndIndex;
+
+    @Test
+    public void testLongestPlateauLength() {
+        int[] values = {5, 5, 4, 2, 3, 3, 3, 3, 3, 1, 2};
+        Plateau plateau = new Plateau(values);
+        assert (plateau.getLongestPlateau().length == longestPlateauLength);
+    }
+
+    @Test
+    public void testLongestPlateauStart() {
+        int[] values = {5, 5, 4, 2, 3, 3, 3, 3, 3, 1, 2};
+        Plateau plateau = new Plateau(values);
+        assert (plateau.getLongestPlateauLocation()[0] == longestPlateauStartIndex);
+    }
+
+    @Test
+    public void testLongestPlateauEnd() {
+        int[] values = {5, 5, 4, 2, 3, 3, 3, 3, 3, 1, 2};
+        Plateau plateau = new Plateau(values);
+        assert (plateau.getLongestPlateauLocation()[plateau.getLongestPlateauLocation().length - 1]
+                == longestPlateauEndIndex);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getTestData() {
+        return Arrays.asList(new Object[][]{
+                {3, 2, 4}, {5, 6, 10}, {7, 3, 9}
+        });
+    }
+
 }
