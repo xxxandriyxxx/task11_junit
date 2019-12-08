94fa26b859ee02bb4ad1a0e7ebba1781d515eded
Andriy <andriymattt@gmail.com>
2019-12-08 23:06:38 +0200

    feature/test: minor changes

--- src/main/java/com/epam/plateau/Main.java
+++ src/main/java/com/epam/plateau/Main.java
@@ -8,8 +8,8 @@ public class Main {
         int[] values = {5, 5, 4, 2, 3, 3, 3, 3, 3, 1, 2};
         Plateau plateau = new Plateau(values);
         System.out.println("Array: " + Arrays.toString(plateau.getArray()));
-        System.out.println("Longest com.epam.plateau: " + Arrays.toString(plateau.getLongestPlateau()));
-        System.out.println("Longest com.epam.plateau location: " + Arrays.toString(plateau.getLongestPlateauLocation()));
-        System.out.println("Longest com.epam.plateau length: " + plateau.getLongestPlateauLength());
+        System.out.println("Longest plateau: " + Arrays.toString(plateau.getLongestPlateau()));
+        System.out.println("Longest plateau location: " + Arrays.toString(plateau.getLongestPlateauLocation()));
+        System.out.println("Longest plateau length: " + plateau.getLongestPlateauLength());
     }
 }
