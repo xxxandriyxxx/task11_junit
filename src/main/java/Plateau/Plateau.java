ddfc1ef70af6542c40ff1b1474f18e2d65055f59
Andriy <andriymattt@gmail.com>
2019-12-08 17:38:55 +0200

    feature/plateau: added class Plateau and wrote method of getting longest plateau of array

---
+++ src/main/java/Plateau/Plateau.java
@@ -0,0 +1,72 @@
+package Plateau;
+
+public class Plateau {
+
+    private int[] array;
+    private int[] longestPlateau;
+    private int longestPlateauLength;
+    private int[] longestPlateauLocation;
+
+    public Plateau(int[] array) {
+        this.array = array;
+        findLongestPlateau();
+    }
+
+    private void findLongestPlateau() {
+        int longestPlateauLength = 0;
+        int plateauStart = 0;
+        int plateauEnd = 0;
+        //if plateau exists it can start anywhere between 0 and length-3. Can't exist after that
+        for (int i = 0; i < array.length - 3; i++) {
+            //plateau cant start at the first 3 locations
+            for (int j = i + 3; j < array.length; j++) {
+                boolean isLess = false;
+                boolean isMiddleHigher = true;
+                //value at the end of the plateau can be less or equal to value at the beginning
+                if (array[j] <= array[i]) {
+                    isLess = true;
+                    //all middle values equal and higher than 1st value
+                    for (int p = i + 2; p < j; p++) {
+                        if ((array[p] != array[p - 1] || array[p] <= array[i])) {
+                            isMiddleHigher = false;
+                            break;
+                        }
+                    }
+                }
+                if (isLess && isMiddleHigher) {
+                    int length = j - i;
+                    if (length >= longestPlateauLength) {
+                        longestPlateauLength = length;
+                        plateauStart = i;
+                        plateauEnd = j;
+                    }
+                }
+            }
+        }
+        if (longestPlateauLength > 0) {
+            longestPlateau = new int[plateauEnd - plateauStart + 1];
+            longestPlateauLocation = new int[plateauEnd - plateauStart + 1];
+            for (int i = plateauStart; i <= plateauEnd; i++) {
+                longestPlateau[i - plateauStart] = array[i];
+                longestPlateauLocation[i - plateauStart] = i;
+            }
+            this.longestPlateauLength = longestPlateauLength + 1;
+        }
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public int[] getLongestPlateau() {
+        return longestPlateau;
+    }
+
+    public int getLongestPlateauLength() {
+        return longestPlateauLength;
+    }
+
+    public int[] getLongestPlateauLocation() {
+        return longestPlateauLocation;
+    }
+}
