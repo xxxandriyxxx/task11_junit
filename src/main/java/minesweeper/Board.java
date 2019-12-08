d24a214025427552562c70ef80194ade1a27331c
Andriy <andriymattt@gmail.com>
2019-12-08 17:58:52 +0200

    feature/minesweeper: add method of setting number of neighboring bombs

--- src/main/java/minesweeper/Board.java
+++ src/main/java/minesweeper/Board.java
@@ -32,5 +32,39 @@ public class Board {
         }
     }
 
+    private void setNeighboringBombs() {
+        int neighboringBombs;
+        for (int i = 0; i < sizeH; i++) {
+            for (int j = 0; j < sizeV; j++) {
+                neighboringBombs = 0;
+                if ((i != 0) && (j != 0) && cells[i - 1][j - 1].isBomb()) {
+                    neighboringBombs++;
+                }
+                if ((i != 0) && cells[i - 1][j].isBomb()) {
+                    neighboringBombs++;
+                }
+                if ((i != 0) && (j != sizeV - 1) && cells[i - 1][j + 1].isBomb()) {
+                    neighboringBombs++;
+                }
+                if ((j != 0) && cells[i][j - 1].isBomb()) {
+                    neighboringBombs++;
+                }
+                if ((j != sizeV - 1) && cells[i][j + 1].isBomb()) {
+                    neighboringBombs++;
+                }
+                if ((i != sizeH - 1) && (j != 0) && cells[i + 1][j - 1].isBomb()) {
+                    neighboringBombs++;
+                }
+                if ((i != sizeH - 1) && cells[i + 1][j].isBomb()) {
+                    neighboringBombs++;
+                }
+                if ((i != sizeH - 1) && (j != sizeV - 1) && cells[i + 1][j + 1].isBomb()) {
+                    neighboringBombs++;
+                }
+                cells[i][j].setNeighboringBombs(cells[i][j].getNeighboringBombs() + neighboringBombs);
+            }
+        }
+    }
+
 
 }
