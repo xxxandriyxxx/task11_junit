4fe3910bbb7040eeec94a6ca8f1a4e874c328863
Andriy <andriymattt@gmail.com>
2019-12-09 00:57:43 +0200

    feature/test: add Calculator interface and MathApp class to use they in tests

---
+++ src/main/java/com/epam/forTests/Calculator.java
@@ -0,0 +1,11 @@
+package com.epam.forTests;
+
+public interface Calculator {
+    int add(int value1, int value2);
+
+    int subtract(int value1, int value2);
+
+    int multiply(int value1, int value2);
+
+    double divede(int value1, int value2);
+}
---
+++ src/main/java/com/epam/forTests/MathApp.java
@@ -0,0 +1,32 @@
+package com.epam.forTests;
+
+public class MathApp {
+    private Calculator calculator;
+
+    public MathApp() {
+    }
+
+    public MathApp(Calculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void setCalculator(Calculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public int add(int value1, int value2) {
+        return calculator.add(value1, value2);
+    }
+
+    public int subtract(int value1, int value2) {
+        return calculator.subtract(value1, value2);
+    }
+
+    public int multiply(int value1, int value2) {
+        return calculator.multiply(value1, value2);
+    }
+
+    public double divede(int value1, int value2) {
+        return calculator.divede(value1, value2);
+    }
+}
