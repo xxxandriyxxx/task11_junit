ed9683418b3cd24e065428cec9d9f59a037d94fd
Andriy <andriymattt@gmail.com>
2019-12-08 22:09:31 +0200

    feature/test: add class TryTest to try how basic tests work

---
+++ src/test/java/com/epam/TryTest.java
@@ -0,0 +1,69 @@
+package com.epam;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+
+public class TryTest {
+
+    private boolean condition = false;
+    private static int count = 0;
+
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("@BeforeAll: count = " + count);
+    }
+
+    @BeforeEach
+    public void beforeTest() {
+        System.out.println("@BeforeEach: count = " + count);
+    }
+
+    @AfterEach
+    public void afterTest() {
+        System.out.println("@AfterEach: count = " + count);
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("@AfterAll: count = " + count);
+    }
+
+    @DisplayName("super test")
+    @Test
+    public void testMethodAsd() {
+        count++;
+        System.out.println("TestMethodAsd works!");
+    }
+
+    @Test
+    @Disabled
+    public void testMethodQwe() {
+        count++;
+        System.out.println("TestMethodQwe works!");
+    }
+
+    @Test
+    public void testMethodAssume() {
+        count++;
+        assumeTrue(condition);
+        System.out.println("TestMethodAssume works!");
+    }
+
+    @Test
+    public void testMethodAssert() {
+        count++;
+        assertTrue(condition);
+        System.out.println("TestMethodAssert works!");
+    }
+
+    @Test
+    public void testMethodFail() {
+        count++;
+        fail("Hello from fail!");
+    }
+
+}
